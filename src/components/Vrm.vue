057d3c13da54dd67054559e2cdfca2293d04e110
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-07-26 08:34:53 +0900

    Vrm.vueの作成

---
+++ src/components/Vrm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="vrm"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Vrm extends Vue {}
+</script>
+
+<style>
+</style>
