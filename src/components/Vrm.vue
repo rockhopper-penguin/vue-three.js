16c3d2441835d1235bbb9722b3797b4845180f10
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-07-28 21:45:39 +0900

    threejsの読み込み

--- src/components/Vrm.vue
+++ src/components/Vrm.vue
@@ -4,6 +4,9 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { VRM } from "@pixiv/three-vrm";
 
 @Component
 export default class Vrm extends Vue {}
